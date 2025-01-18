4ce371fddfc1eda2f11d3d6d4f544e33c70891bb
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-01-17 23:58:12 -0400

    creacion del contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using RegistroTecnicos.Models;
+namespace RegistroTecnicos.DAL;
+
+public class Contexto(DbContextOptions<Contexto> options) : DbContext(options)
+{
+    public DbSet<Tecnicos> Tecnicos { get; set; }
+}
