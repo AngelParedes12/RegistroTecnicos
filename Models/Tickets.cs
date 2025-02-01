8a5ca16b59653f766e6c671e9f7c14fa1f87131a
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-02-01 16:37:51 -0400

    Modelos de tickets

--- Models/Tickets.cs
+++ Models/Tickets.cs
@@ -1,34 +1,47 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using static RegistroTecnicos.Components.Pages.Clientes.ClienteEdit;
 
 namespace RegistroTecnicos.Models
 {
+    public enum Prioridad
+    {
+        Baja,
+        Media,
+        Alta,
+        Urgente
+    }
 
-    
-        public class Ticket
-        {
-            [Key]
-            public int TicketId { get; set; }
+    public class Tickets
+    {
+        [Key]
+        public int TicketId { get; set; }
 
-            [Required]
-            public DateTime Fecha { get; set; } = DateTime.Now;
+        [Required(ErrorMessage = "La fecha es requerida.")]
+        public DateTime Fecha { get; set; } = DateTime.Now;
 
-            [Required]
-            public string Prioridad { get; set; } = "Media";
+        [Required(ErrorMessage = "La prioridad es requerida.")]
+        public Prioridad Prioridad { get; set; } = Prioridad.Media;
 
-            [Required]
-            public int ClienteId { get; set; }
+        [Required(ErrorMessage = "El ClienteId es requerido.")]
+        [Range(1, int.MaxValue, ErrorMessage = "El ClienteId debe ser mayor que 0.")]
+        public int ClienteId { get; set; }
 
-            [Required]
-            [StringLength(100)]
-            public string Asunto { get; set; }
+        public Clientes? Cliente { get; set; } // Propiedad de navegación
 
-            [Required]
-            public string Descripcion { get; set; }
+        [Required(ErrorMessage = "El asunto es requerido.")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "El asunto debe tener entre 5 y 100 caracteres.")]
+        public string Asunto { get; set; }
 
-            public double TiempoInvertido { get; set; }
+        [Required(ErrorMessage = "La descripción es requerida.")]
+        [StringLength(500, MinimumLength = 10, ErrorMessage = "La descripción debe tener entre 10 y 500 caracteres.")]
+        public string Descripcion { get; set; }
 
-            public int? TecnicoId { get; set; }
-        }
- 
- }
+        [Range(0, double.MaxValue, ErrorMessage = "El tiempo invertido no puede ser negativo.")]
+        public double TiempoInvertido { get; set; }
 
+        [Range(1, int.MaxValue, ErrorMessage = "El TecnicoId debe ser mayor que 0.")]
+        public int? TecnicoId { get; set; }
+
+        public Tecnicos? Tecnico { get; set; } // Propiedad de navegación
+    }
+}
