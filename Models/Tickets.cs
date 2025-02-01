ec63bcfc4d1a25367feebc4350a7deda1b5cf9dc
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-02-01 15:07:28 -0400

    Añadiendo el contexto de Tickets

---
+++ Models/Tickets.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+
+    
+        public class Ticket
+        {
+            [Key]
+            public int TicketId { get; set; }
+
+            [Required]
+            public DateTime Fecha { get; set; } = DateTime.Now;
+
+            [Required]
+            public string Prioridad { get; set; } = "Media";
+
+            [Required]
+            public int ClienteId { get; set; }
+
+            [Required]
+            [StringLength(100)]
+            public string Asunto { get; set; }
+
+            [Required]
+            public string Descripcion { get; set; }
+
+            public double TiempoInvertido { get; set; }
+
+            public int? TecnicoId { get; set; }
+        }
+ 
+ }
+
